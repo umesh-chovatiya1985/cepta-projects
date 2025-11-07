3bf06acc6af83a18e33836d04169a101c97b7123
Umesh Chovatiya <umesh.chovatiya1985@gmail.com>
2025-11-07 16:39:48 +0530

    Latest

--- react-vite-routing/src/pages/Home.tsx
+++ react-vite-routing/src/pages/Home.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import Navigation from '../components/Navigation'
-import Alert from '../components/Alert'
+import { useEffect, useState } from "react"
+import Navigation from "../components/Navigation"
+import Alert from "../components/Alert"
 
 export default function Home() {
+  const [isAlert, setIsAlert] = useState(false)
 
-    const navigate = useNavigate()
+  useEffect(() => {
+    console.log("isAlert has changed " + isAlert);
+    // Componnt will unmount.
+    return () => {
+
+    }
+  }, [isAlert]);
 
   return (
     <>
-        <Navigation />
-        <div>Home</div>
-        <button onClick={() => navigate('/products')}>Click to go products</button>
+      <Navigation />
+      <div>Home</div>
+      <button onClick={() => setIsAlert(!isAlert)}>Click to go products</button>
+      {isAlert ? (
         <Alert type="fail">
           <div>This is example to pass a children in component</div>
         </Alert>
+      ) : (
+        <div>No Alert</div>
+      )}
     </>
   )
 }
