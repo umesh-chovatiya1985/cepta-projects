9bda056dca42cf693267743f2211784ab17bc8c2
Umesh Chovatiya <umesh.chovatiya1985@gmail.com>
2025-11-11 14:25:46 +0530

    Project add on 11-11-2025

--- react-with-shadcn/src/components/TextBox.tsx
+++
@@ -1,7 +0,0 @@
-export default function TextBox(props: any) {
-  return (
-    <div>
-      <input {...props} className="border rounded px-2 py-1" />
-    </div>
-  )
-}
---
+++ shad-cn-ui/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import "./App.css"
+import { Button } from "./components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
